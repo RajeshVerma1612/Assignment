0b63217f0344beb66caf6cf1d36c007e622630d7
RajeshVerma1612 <verma.rajesh123v@gmail.com>
2023-01-29 21:04:02 +0530

    set d or some flie

--- C_13/9.c
+++ C_13/9.c
@@ -7,55 +7,95 @@ typedef struct student
     float marks;
     char dob[10];
 } Student;
-int main()
-{
-    int ch;
-    FILE *fp;
-    fp=fopen("student.dat","wb");
+void add_record(FILE *fp){
     Student s;
-    system("cls");
     printf("Enter the Student Name :");
     gets(s.name);
     printf("\nEnter the Roll No :");
     scanf("%d", &s.roll);
     printf("\nEnter the marks :");
     scanf("%f", &s.marks);
-    printf("\nEnter the DOB");
+    printf("\nEnter the DOB :");
     fflush(stdin);
     gets(s.dob);
-    fwrite(&s, sizeof(s), 1, fp);
+    fseek(fp, 0, SEEK_END);
+    fwrite(&s, sizeof(Student), 1, fp);
     fclose(fp);
-    // while (1)
-    // {
-    //     printf("1) Create a file\n2) Add a record\n3) Delete a record\n4) Modify the marks by add grace marks\n5) Exit\n");
-    //     printf("Enter Your Choice :\n");
-    //     scanf("%d", ch);
-    //     switch (ch)
-    //     {
-    //     case 1:
-    //         fp = fopen("student.dat", "rb+");
-    //         if (fp == NULL)
-    //         {
-    //             printf("File not created\n");
-    //         }
-    //         else
-    //         {
-    //             printf("File is created\n");
-    //         }
-    //         break;
-    //     case 2:
-    //         // fflush(stdin);
+}
+void del_record(FILE *fp){
+    Student s[1000];
+    int roll;
+    printf("Enter the Roll No. :\n");
+    scanf("%d",&roll);
+    fseek(fp, 0, SEEK_SET);
+    int i=0;
+    while(fread(&s[i],sizeof(Student),1,fp)){
+        i++;
+    }
+    int is = 0;
+   for (int j = 0; j < i; j++)
+   {
+      if (s[j].roll == roll)
+      {
+         s[i].roll = -1;
+         is= 1;
+         break;
+      }
+   }
+   if(is == 0)
+   {
+      printf("Student not found...\n");
+   }
+   else{
+      printf("Record deleted successfully\n");
 
-    //         break;
-    //     case 3:
+   }
+   fseek(fp, 0, SEEK_SET);
+   for (int j = 0; j < i; j++)
+   {
+         fwrite(&s[j], sizeof(Student), 1, fp);
 
-    //     case 4:
+   }
+}
+int main()
+{
+    int ch;
+    system("cls");
+    FILE *fp;
+    // fp=fopen("student.dat","wb+");
+    while (1)
+    {
+        printf("1) Create a file\n2) Add a record\n3) Delete a record\n4) Modify the marks by add grace marks\n5) Exit\n");
+        printf("Enter Your Choice :\n");
+        scanf("%d",&ch);
+        switch (ch)
+        {
+        case 1:
+            fp = fopen("student.dat", "rb+");
+            if (fp == NULL)
+            {
+                printf("File not created\n");
+            }
+            else
+            {
+                printf("File is created\n");
+            }
+            break;
+        case 2:
+            fflush(stdin);
+            add_record(fp);
+            printf("Record Added\n");
+            break;
+        case 3:
+            del_record(fp);
+            break;
+        case 4:
 
-    //     case 5:
-
-    //     default:
-    //         printf("Enter a valid choice\n");
-    //     }
-    // }
+        case 5:
+            return 0;
+        default:
+            printf("Enter a valid choice\n");
+        }
+    }
     return 0;
 }
